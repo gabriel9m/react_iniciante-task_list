53151f331ba189cfbbef9e520101da3a76c77883
Gabriel Menezes <61811843+gabriel9m@users.noreply.github.com>
2021-02-07 19:49:35 -0300

    Initial commit

---
+++ src/components/TaskList/TaskList.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./taskList.css";
+import PropTypes from "prop-types";
+
+import TaskItem from "../TaskItem/TaskItem";
+
+export default function TaskList({
+  title,
+  taskState,
+  onAddTask,
+  tasks,
+  onTaskUpdate,
+  onDeleteTask
+}) {
+  const addTask = () => {
+    onAddTask("Nova Tarefa", taskState);
+  };
+
+  return (
+    <div className="taskList">
+      <div className="title">{title}</div>
+      <div clasName="content">
+        {tasks.map((task) => {
+          return (
+            <TaskItem
+              key={task.id}
+              id={task.id}
+              title={task.title}
+              taskState={task.state}
+              onTaskUpdate={onTaskUpdate}
+              onDeleteTask={onDeleteTask}
+            />
+          );
+        })}
+      </div>
+      <button onClick={addTask} className="btn">
+        Adicionar Tarefa
+      </button>
+    </div>
+  );
+}
+
+TaskList.propTypes = {
+  title: PropTypes.string.isRequired,
+  onAddTask: PropTypes.func.isRequired,
+  tasks: PropTypes.array.isRequired
+};
